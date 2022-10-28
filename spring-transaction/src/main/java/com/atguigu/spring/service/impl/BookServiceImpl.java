8f60abac31798e72b2655397819e6c1ea7a57a45
mark <fafuxu@163.com>
2022-10-28 20:20:03 +0800

    Changes

---
+++ spring-transaction/src/main/java/com/atguigu/spring/service/impl/BookServiceImpl.java
@@ -0,0 +1,38 @@
+package com.atguigu.spring.service.impl;
+
+import com.atguigu.spring.dao.BookDao;
+import com.atguigu.spring.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookDao bookDao;
+
+    @Override
+    @Transactional(
+//            readOnly = false,
+//            timeout = 3
+            noRollbackFor = {java.lang.ArithmeticException.class}
+    )
+    //事务管理注解，使其保持原子性
+    public void buyBook(Integer userId, Integer bookId) {
+//        try {
+//            TimeUnit.SECONDS.sleep(5);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        //查询图书价格
+        Integer price = bookDao.getPriceByBookId(bookId);
+
+        //更新图书库存
+        bookDao.updateStock(bookId);
+//        更新用户余额
+        bookDao.updateBalance(userId, price);
+        System.out.println(1/0);
+    }
+}
--- spring_aop/src/main/java/com/atguigu/spring/aop/annotation/LoggerAspect.java
+++ spring_aop/src/main/java/com/atguigu/spring/aop/annotation/LoggerAspect.java
@@ -1,15 +1,22 @@
 package com.atguigu.spring.aop.annotation;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @Aspect //将当前组件设置为切面
 public class LoggerAspect {
-
-    @Before("execution(public int com.atguigu.spring.aop.annotation.CalculatorImpl.add(int,int))")
-    public void beforeAdviseMethod(){
-        System.out.println("前置通知");
+    @Pointcut("execution(* com.atguigu.spring.aop.annotation.CalculatorImpl.*(..))")
+    public void pointCut(){}
+    @Before("pointCut()")//重用切入点表达式
+    //@Before("execution(* com.atguigu.spring.aop.annotation.*.*(..))")
+    //@Before("execution(public int com.atguigu.spring.aop.annotation.CalculatorImpl.add(int,int))")
+    public void beforeAdviseMethod(JoinPoint joinPoint) {
+        System.out.println("前置通知" + joinPoint.getSignature().getName() + Arrays.toString(joinPoint.getArgs()));
     }
 }
